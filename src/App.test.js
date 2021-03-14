3fd743d6c588806f27f4657d45f9da86c87a6c95
Attila Eross <attila.eross@yahoo.co.uk>
2021-03-14 15:06:27 +0000

    removed unnecessary elements and relating styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders empty todo app with heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Todo App React/i);
   expect(linkElement).toBeInTheDocument();
 });

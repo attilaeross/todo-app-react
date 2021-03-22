752d7b48d72151f9f09ff026a615826cad4bb299
Attila Eross <attila.eross@yahoo.co.uk>
2021-03-22 20:53:23 +0000

    added basic elements and styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Todo App React</h1>
-      </header>
+      <div id="root">
+        <input
+          type="text"
+          className="user-input"
+          placeholder="Please enter username"
+        />
+        <button className="change-user">Set User</button>
+        <header>
+          <h1 className="header">Todo App</h1>
+        </header>
+        <form>
+          <input
+            type="text"
+            className="new-todo"
+            placeholder="Please enter todo here"
+          />
+          <button className="add">Add</button>
+          <select>
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="outstanding">Outstanding</option>
+          </select>
+        </form>
+        <h2 className="list-header"> </h2>
+        <ul className="list"></ul>
+      </div>
     </div>
   );
 }
